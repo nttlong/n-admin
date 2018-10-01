5b4f03887a0d6cdcc3dd5f0ce638798ce4af3d4c
nttlong <zugeliang2000@gmail.com>
2018-10-01 11:08:10 +0700

    save

---
+++ server.js
@@ -0,0 +1,18 @@
+var q = require("quicky");
+var cnn = "mongodb://root:123456@localhost/hrm";
+q.language.setConfig(cnn,"sys_language");
+var app = q.apps;
+app.setSecretKey("sas03udh74327%$63283");
+app.setCacheMode(true);
+app.setCompressMode(false);
+app.sessionCacheUseMemCache(true);
+app.sessionCacheUseMemCache(false);
+app.load(
+    {
+        name:"admin",
+        hostDir:"",
+        dir:"apps/admin"
+    }
+).listen(3000,function(){
+    console.log("app start  at port 3000");
+});
