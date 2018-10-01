38d1d43a5e68aaf606f1dde5257ccee6142d79b5
nttlong <zugeliang2000@gmail.com>
2018-10-01 16:23:38 +0700

    save

--- apps/admin/actions/auth.js
+++
@@ -1,5 +0,0 @@
-var q=require("quicky");
-function login(username,password){
-    var ret=q.sys.login(username,password);
-    return ret;
-}
---
+++ apps/admin/libs/menu.js
@@ -0,0 +1,22 @@
+var q=require("quicky/q-language");
+module.exports =[
+    {
+        caption:"System",
+        items:[
+            {
+                caption:"Users",
+                page:"page/users"
+            },{
+                caption:"Customers",
+                page:"page/custemers"
+            }
+        ]
+        
+    },{
+        caption:"Linquictisc",
+        items :[
+            
+        ]
+
+    }
+];
